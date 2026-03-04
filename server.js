7f0aea0a30266b87ea042887981344a16f19b228
imkhan89 <info@ndestore.com>
2026-03-04 20:02:00 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import express from "express";
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("NDE AI SERVER RUNNING");
+});
+
+app.post("/webhook", (req, res) => {
+
+  const incomingMsg = req.body.Body;
+
+  console.log("Incoming message:", incomingMsg);
+
+  let reply = "Hello from NDE AI Assistant";
+
+  res.type("text/xml");
+  res.send(`
+<Response>
+<Message>${reply}</Message>
+</Response>
+`);
+
 });
 
 const PORT = process.env.PORT || 3000;
