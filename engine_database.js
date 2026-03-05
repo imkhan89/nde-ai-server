262399471ad6b42c405e075a763fc80f6647d764
imkhan89 <info@ndestore.com>
2026-03-05 23:04:13 +0500

    Create engine_database.js

---
+++ engine_database.js
@@ -0,0 +1,47 @@
+/* ENGINE DATABASE */
+
+const ENGINE_DATABASE = {
+
+"2ZR-FE": {
+manufacturer: "Toyota",
+vehicles: [
+"Corolla 2007-2019",
+"Toyota Prius",
+"Toyota Auris"
+],
+cc: 1800
+},
+
+"1GD-FTV": {
+manufacturer: "Toyota",
+vehicles: [
+"Hilux Revo",
+"Fortuner",
+"Land Cruiser Prado"
+],
+cc: 2800
+},
+
+"R18A": {
+manufacturer: "Honda",
+vehicles: [
+"Civic FD",
+"CRV",
+"Stream"
+],
+cc: 1800
+},
+
+"K12B": {
+manufacturer: "Suzuki",
+vehicles: [
+"Swift",
+"Dzire",
+"WagonR"
+],
+cc: 1200
+}
+
+};
+
+module.exports = ENGINE_DATABASE;
