4f0962553eae3153de5af27f43fa44de8ce369b0
imkhan89 <info@ndestore.com>
2026-03-05 19:18:16 +0500

    Create searchEngine.js

---
+++ searchEngine.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+const INDEX = JSON.parse(
+fs.readFileSync("./data/search_index.json")
+);
+
+function tokenize(text){
+
+return text
+.toLowerCase()
+.replace(/[^a-z0-9 ]/g,"")
+.split(" ")
+.filter(w=>w.length>2);
+
+}
+
+function searchProducts(query){
+
+const tokens = tokenize(query);
+
+let results = [];
+
+for(const token of tokens){
+
+if(INDEX[token]){
+
+results.push(...INDEX[token]);
+
+}
+
+}
+
+const unique = {};
+const final = [];
+
+for(const r of results){
+
+if(!unique[r.id]){
+
+unique[r.id] = true;
+final.push(r);
+
+}
+
+}
+
+return final.slice(0,10);
+
+}
+
+module.exports = {searchProducts};
