606caa950f514763acf98609bb893ccb36eefd57
imkhan89 <info@ndestore.com>
2026-03-05 23:02:48 +0500

    Create vehicle_database.js

---
+++ vehicle_database.js
@@ -0,0 +1,511 @@
+const VEHICLE_DATABASE = {
+
+/* =========================
+TOYOTA
+(Page 1-4)
+========================= */
+
+toyota:{
+
+corolla:[
+"corolla 1988-2001",
+"corolla 2002-2008",
+"corolla 2009-2014",
+"corolla 2015-2017",
+"corolla 2018-2023"
+],
+
+yaris:[
+"yaris 2006-2013",
+"yaris 2014-2020",
+"yaris 2020+"
+],
+
+hilux:[
+"hilux vigo",
+"hilux revo"
+],
+
+fortuner:[
+"fortuner",
+"fortuner legender"
+],
+
+aqua:[
+"aqua 2012-2014",
+"aqua 2015-2017",
+"aqua 2018-2021"
+],
+
+vitz:[
+"vitz 2005-2010",
+"vitz 2011-2016",
+"vitz 2017-2019"
+],
+
+raize:[
+"raize"
+],
+
+landcruiser:[
+"land cruiser 100",
+"land cruiser 200",
+"land cruiser 300"
+],
+
+prado:[
+"prado 120",
+"prado 150"
+],
+
+c_hr:[
+"c-hr"
+],
+
+mark_x:[
+"mark x"
+],
+
+probox:[
+"probox"
+],
+
+rush:[
+"rush"
+],
+
+corolla_cross:[
+"corolla cross"
+]
+
+},
+
+/* =========================
+HONDA
+(Page 5-6)
+========================= */
+
+honda:{
+
+accord:[
+"accord 2003-2008",
+"accord 2009-2013"
+],
+
+city:[
+"city 2003-2008",
+"city 2009-2013",
+"city 2014-2021",
+"city 2022+"
+],
+
+civic:[
+"civic 1996-2000",
+"civic 2001-2005",
+"civic 2006-2012",
+"civic 2013-2016",
+"civic 2017-2021",
+"civic 2022+"
+],
+
+brv:[
+"brv"
+],
+
+hrv:[
+"hr-v"
+],
+
+vezel:[
+"vezel"
+],
+
+n_series:[
+"n box",
+"n one"
+],
+
+civic_x:[
+"civic x"
+],
+
+fit:[
+"fit"
+],
+
+freed:[
+"freed"
+],
+
+insight:[
+"insight"
+],
+
+jazz:[
+"jazz"
+],
+
+life:[
+"life"
+],
+
+airwave:[
+"airwave"
+],
+
+brio:[
+"brio"
+],
+
+crz:[
+"crz"
+],
+
+element:[
+"element"
+],
+
+freed_spike:[
+"freed spike"
+],
+
+crossroad:[
+"crossroad"
+]
+
+},
+
+/* =========================
+SUZUKI
+(Page 6-8)
+========================= */
+
+suzuki:{
+
+cultus:[
+"cultus 2000-2007",
+"cultus 2008-2016",
+"cultus 2017+"
+],
+
+swift:[
+"swift 2005-2010",
+"swift 2011-2017",
+"swift 2018+"
+],
+
+wagonr:[
+"wagon r"
+],
+
+mehran:[
+"mehran"
+],
+
+alto:[
+"alto 660",
+"alto 1000",
+"alto 2019+"
+],
+
+every:[
+"every"
+],
+
+bolan:[
+"bolan"
+],
+
+baleno:[
+"baleno"
+],
+
+vitara:[
+"vitara"
+]
+
+},
+
+/* =========================
+HYUNDAI
+(Page 8)
+========================= */
+
+hyundai:{
+
+elantra:[
+"elantra"
+],
+
+tucson:[
+"tucson"
+],
+
+santafe:[
+"santa fe"
+],
+
+sonata:[
+"sonata"
+],
+
+santro:[
+"santro"
+],
+
+grand_i10:[
+"grand i10"
+]
+
+},
+
+/* =========================
+KIA
+(Page 8-9)
+========================= */
+
+kia:{
+
+sportage:[
+"sportage"
+],
+
+stonic:[
+"stonic"
+],
+
+sorento:[
+"sorento"
+],
+
+grand_carnival:[
+"grand carnival"
+]
+
+},
+
+/* =========================
+DAIHATSU
+(Page 9)
+========================= */
+
+daihatsu:{
+
+cuore:["cuore"],
+esse:["esse"],
+move:["move"],
+boon:["boon"],
+cast:["cast"],
+copen:["copen"],
+mira:["mira"]
+},
+
+/* =========================
+NISSAN
+(Page 9-10)
+========================= */
+
+nissan:{
+
+dayz:[
+"dayz"
+],
+
+juke:[
+"juke"
+],
+
+leaf:[
+"leaf"
+],
+
+note:[
+"note"
+],
+
+wingroad:[
+"wingroad"
+]
+
+},
+
+/* =========================
+MITSUBISHI
+(Page 10)
+========================= */
+
+mitsubishi:{
+
+lancer:["lancer"],
+ek_wagon:["ek wagon"],
+ek_custom:["ek custom"]
+},
+
+/* =========================
+LEXUS
+(Page 10-11)
+========================= */
+
+lexus:{
+
+ct:["ct"],
+es:["es"],
+hs:["hs"],
+ls:["ls"],
+rx:["rx"],
+lx:["lx"],
+gx:["gx"]
+},
+
+/* =========================
+EUROPEAN BRANDS
+(Page 11-13)
+========================= */
+
+bmw:{
+
+series_3:["3 series"],
+series_5:["5 series"],
+series_7:["7 series"],
+x1:["x1"],
+x3:["x3"],
+x5:["x5"]
+},
+
+audi:{
+
+a4:["a4"],
+a6:["a6"],
+q5:["q5"],
+q7:["q7"]
+},
+
+mercedes:{
+
+c_class:["c class"],
+e_class:["e class"],
+s_class:["s class"],
+ml_class:["ml class"],
+gl_class:["gl class"]
+},
+
+ford:{
+f150:["f150"],
+focus:["focus"]
+},
+
+peugeot:{
+208:["208"]
+},
+
+land_rover:{
+discovery:["discovery 4"]
+},
+
+changan:{
+alvin:["alvin"],
+karvaan:["karvaan"]
+},
+
+/* =========================
+MOTORCYCLES
+(Page 13)
+========================= */
+
+motorcycles:{
+
+honda:[
+"cd70",
+"cg125",
+"150cc"
+],
+
+yamaha:[
+"ybr125"
+],
+
+suzuki:[
+"gd110",
+"gs150"
+]
+
+},
+
+/* =========================
+DECALS
+(Page 13-14)
+========================= */
+
+decals:{
+
+anime:[
+"anime decals"
+],
+
+funny:[
+"funny stickers"
+],
+
+doctor:[
+"doctor stickers"
+],
+
+hunter:[
+"hunter stickers"
+],
+
+toyota:[
+"toyota stickers"
+],
+
+honda:[
+"honda stickers"
+],
+
+sports:[
+"sports mind stickers"
+],
+
+door_sill:[
+"door sill stickers"
+],
+
+laptop:[
+"laptop stickers"
+],
+
+custom:[
+"custom decals"
+],
+
+jeep:[
+"jeep stickers"
+],
+
+firearm:[
+"firearm stickers"
+],
+
+gtr:[
+"gtr stickers"
+]
+
+},
+
+/* =========================
+ACCESSORIES
+(Page 14)
+========================= */
+
+accessories:{
+
+general:[
+"storage box",
+"tire nozzle cap"
+]
+
+}
+
+};
+
+module.exports = VEHICLE_DATABASE;
