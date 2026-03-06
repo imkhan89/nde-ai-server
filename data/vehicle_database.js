58bb5dc7d9761c9d9c2d6c0f9a965c6622f969d6
imkhan89 <info@ndestore.com>
2026-03-06 20:23:54 +0500

    Create vehicle_database.js

---
+++ data/vehicle_database.js
@@ -0,0 +1,73 @@
+/* =====================================================
+ndestore.com GLOBAL VEHICLE DATABASE
+Scalable Automotive Model Library
+===================================================== */
+
+module.exports = {
+
+toyota:[
+"corolla","camry","yaris","vitz","aqua","prius","hilux","fortuner",
+"land cruiser","prado","raize","rush","premio","mark x","chr","noah","voxy"
+],
+
+honda:[
+"civic","city","accord","vezel","brv","hrv","jazz","fit","crv","nbox"
+],
+
+suzuki:[
+"alto","mehran","cultus","swift","wagon r","bolan","every","ciaz","carry"
+],
+
+hyundai:[
+"elantra","sonata","tucson","santa fe","santro","accent","kona"
+],
+
+kia:[
+"sportage","picanto","rio","cerato","sorento","stonic"
+],
+
+nissan:[
+"note","juke","micra","sunny","xtrail","patrol","dayz"
+],
+
+mitsubishi:[
+"lancer","mirage","pajero","outlander","ek wagon"
+],
+
+daihatsu:[
+"mira","move","boon","terios","cuore"
+],
+
+bmw:[
+"1 series","3 series","5 series","7 series","x1","x3","x5","x7"
+],
+
+mercedes:[
+"c class","e class","s class","gla","glc","gle","gls"
+],
+
+audi:[
+"a3","a4","a5","a6","a7","a8","q3","q5","q7"
+],
+
+ford:[
+"focus","fiesta","mustang","explorer","escape"
+],
+
+chevrolet:[
+"cruze","spark","malibu","captiva","trailblazer"
+],
+
+volkswagen:[
+"golf","polo","passat","tiguan","touareg"
+],
+
+peugeot:[
+"206","207","208","301","3008"
+],
+
+land_rover:[
+"defender","discovery","range rover","range rover sport"
+]
+
+};
