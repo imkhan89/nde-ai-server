b6dc24552513a85e5eb16972653533db18ff63a4
imkhan89 <info@ndestore.com>
2026-03-06 20:24:42 +0500

    Create part_database.js

---
+++ data/part_database.js
@@ -0,0 +1,59 @@
+/* =====================================================
+ndestore.com GLOBAL PART DATABASE
+===================================================== */
+
+module.exports = [
+
+"brake pad",
+"brake rotor",
+"brake disc",
+"brake shoe",
+
+"air filter",
+"oil filter",
+"cabin filter",
+
+"spark plug",
+"ignition coil",
+
+"radiator",
+"radiator cap",
+"radiator fan",
+"radiator coolant",
+
+"alternator",
+"starter motor",
+"fuel pump",
+
+"battery",
+
+"wiper blade",
+
+"engine shield",
+"fender shield",
+
+"floor mat",
+"trunk mat",
+
+"sun shade",
+"car top cover",
+
+"front bumper",
+"rear bumper",
+"bumper",
+
+"headlight",
+"tail light",
+"fog light",
+
+"side mirror",
+
+"door handle",
+"window switch",
+
+"emblem",
+"monogram",
+
+"car decal"
+
+];
