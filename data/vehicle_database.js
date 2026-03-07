6e12326e4fe91acb06c4fdb4a902c943558d39f4
imkhan89 <info@ndestore.com>
2026-03-07 21:03:54 +0500

    Update vehicle_database.js

--- data/vehicle_database.js
+++ data/vehicle_database.js
@@ -1,73 +1,159 @@
 /* =====================================================
 ndestore.com GLOBAL VEHICLE DATABASE
-Scalable Automotive Model Library
+Optimized for Pakistan + JDM Imports
+AI Parsing Optimized
 ===================================================== */
 
 module.exports = {
 
 toyota:[
-"corolla","camry","yaris","vitz","aqua","prius","hilux","fortuner",
-"land cruiser","prado","raize","rush","premio","mark x","chr","noah","voxy"
+"corolla","camry","yaris","vitz","belta","platz",
+"aqua","prius","prius alpha","prius c",
+"hilux","fortuner","land cruiser","prado",
+"raize","rush","rav4","c-hr",
+"premio","allion","mark x","crown",
+"passo","roomy",
+"noah","voxy","estima","alphard",
+"hiace"
 ],
 
 honda:[
-"civic","city","accord","vezel","brv","hrv","jazz","fit","crv","nbox"
+"civic","city","accord",
+"vezel","hrv","crv","brv",
+"fit","jazz","freed",
+"insight",
+"n box","n one","n wagon",
+"odyssey","stream"
 ],
 
 suzuki:[
-"alto","mehran","cultus","swift","wagon r","bolan","every","ciaz","carry"
-],
-
-hyundai:[
-"elantra","sonata","tucson","santa fe","santro","accent","kona"
-],
-
-kia:[
-"sportage","picanto","rio","cerato","sorento","stonic"
+"alto","mehran","cultus","swift",
+"wagon r","wagon r stingray",
+"bolan","ravi","carry",
+"every","every wagon",
+"ciaz","liana","baleno",
+"ignis","hustler","jimny",
+"spacia","solio"
 ],
 
 nissan:[
-"note","juke","micra","sunny","xtrail","patrol","dayz"
+"sunny","note","march","micra",
+"juke","dayz","moco",
+"x trail","rogue","patrol",
+"teana","bluebird","wingroad",
+"serena"
 ],
 
 mitsubishi:[
-"lancer","mirage","pajero","outlander","ek wagon"
+"lancer","mirage","attrage",
+"pajero","pajero mini","pajero io",
+"outlander","ek wagon","ek space"
 ],
 
 daihatsu:[
-"mira","move","boon","terios","cuore"
+"mira","move","cuore",
+"boon","cast",
+"terios","rocky","tanto"
+],
+
+subaru:[
+"impreza","forester","legacy",
+"xv","wrx","outback","brz"
+],
+
+mazda:[
+"axela","atenza","demio",
+"mazda2","mazda3","mazda6",
+"cx3","cx5","cx8",
+"rx8","roadster"
+],
+
+hyundai:[
+"santro","accent",
+"elantra","sonata",
+"tucson","santa fe",
+"kona","ioniq","venue"
+],
+
+kia:[
+"sportage","picanto","rio",
+"cerato","sorento","stonic","niro"
+],
+
+changan:[
+"alsvin","karvaan",
+"oshan x7","cs35","cs75"
+],
+
+mg:[
+"zs","hs","hs phev",
+"mg3","mg4"
+],
+
+proton:[
+"saga","persona","x70"
+],
+
+dfsk:[
+"glory 580","glory 500",
+"k01","k07"
+],
+
+haval:[
+"h6","jolion","h9"
+],
+
+isuzu:[
+"d max","mu x"
 ],
 
 bmw:[
-"1 series","3 series","5 series","7 series","x1","x3","x5","x7"
+"1 series","2 series","3 series","4 series",
+"5 series","7 series",
+"x1","x2","x3","x4","x5","x6","x7"
 ],
 
 mercedes:[
-"c class","e class","s class","gla","glc","gle","gls"
+"a class","b class",
+"c class","e class","s class",
+"cla","cls",
+"gla","glb","glc","gle","gls",
+"g class"
 ],
 
 audi:[
-"a3","a4","a5","a6","a7","a8","q3","q5","q7"
+"a3","a4","a5","a6","a7","a8",
+"q2","q3","q5","q7","q8"
 ],
 
 ford:[
-"focus","fiesta","mustang","explorer","escape"
+"focus","fiesta",
+"escape","explorer",
+"mustang","ranger"
 ],
 
 chevrolet:[
-"cruze","spark","malibu","captiva","trailblazer"
+"spark","joy",
+"cruze","malibu",
+"captiva","trailblazer"
 ],
 
 volkswagen:[
-"golf","polo","passat","tiguan","touareg"
+"golf","polo","passat",
+"tiguan","touareg"
 ],
 
 peugeot:[
-"206","207","208","301","3008"
+"206","207","208",
+"301","3008","5008"
 ],
 
 land_rover:[
-"defender","discovery","range rover","range rover sport"
+"defender","discovery",
+"discovery sport",
+"range rover",
+"range rover sport",
+"range rover evoque"
 ]
 
 };
