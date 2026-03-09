90b51812a7eb60cd7a7b41395f43729c934e6286
imkhan89 <info@ndestore.com>
2026-03-09 05:35:42 +0500

    Create fitment_database.js

---
+++ data/fitment_database.js
@@ -0,0 +1,43 @@
+module.exports = {
+
+Toyota: {
+
+Corolla: {
+
+2012: {
+engine: "1NZ-FE",
+parts: {
+oil_filter: [
+"04152-YZZA1",
+"Denso 150-3020",
+"Bosch 0986AF0051"
+],
+air_filter: [
+"17801-21060",
+"Denso 143-3701"
+],
+spark_plug: [
+"NGK IFR5A11",
+"Denso SK16HR11"
+]
+}
+},
+
+2015: {
+engine: "1NZ-FE",
+parts: {
+oil_filter: [
+"04152-YZZA1",
+"Denso 150-3020"
+],
+air_filter: [
+"17801-21060"
+]
+}
+}
+
+}
+
+}
+
+}
