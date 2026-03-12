f7d8e6bb98063ee3e28fcc9f5c93746a1f14ea19
imkhan89 <info@ndestore.com>
2026-03-12 07:41:35 +0500

    Update chat_handler.js

--- ai/chat_handler.js
+++ ai/chat_handler.js
@@ -2,10 +2,15 @@ const correctSpelling = require("./spelling_intelligence_engine")
 const parseAutoParts = require("./auto_parts_parser")
 const buildSearchQuery = require("./search_query_builder")
 const buildPartsResponse = require("./response_builder")
+const learning = require("./learning_engine")
 
 function handleChat(message){
 
 if(!message) return null
+
+/* RECORD QUERY */
+
+learning.learn(message)
 
 /* SPELLING CORRECTION */
 
