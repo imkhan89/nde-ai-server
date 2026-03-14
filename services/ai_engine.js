fd796e021ad7fe0a476d84ab1251d0d54523c335
imkhan89 <info@ndestore.com>
2026-03-15 02:31:25 +0500

    Update ai_engine.js

--- services/ai_engine.js
+++ services/ai_engine.js
@@ -1,39 +1,45 @@
-const { detectVehicle } = require('./vehicle_parser');
-const { detectYear } = require('./year_parser');
-const { detectPart } = require('./part_parser');
-const { searchProducts } = require('./product_search');
+import { detectVehicle } from "./vehicle_parser.js"
+import { detectYear } from "./year_parser.js"
+import { detectPart } from "./part_parser.js"
+import { searchProducts } from "./product_search.js"
+import { getFitment } from "./fitment_engine.js"
 
-async function processMessage(message) {
+export async function processMessage(message) {
 
-  const vehicle = detectVehicle(message);
-  const year = detectYear(message);
-  const part = detectPart(message);
+  const vehicle = detectVehicle(message)
+  const year = detectYear(message)
+  const part = detectPart(message)
 
   if (!part) {
-    return "Please tell us which auto part you need.";
+    return "Please tell us which auto part you need."
   }
 
-  const products = await searchProducts(part);
+  const fitment = getFitment(vehicle, year, part)
 
-  if (!products.length) {
-    return "Sorry, we couldn't find that product.";
-  }
+  const products = await searchProducts(part)
 
-  let response = "";
+  let response = ""
 
   if (vehicle && year) {
-    response += `For ${vehicle} ${year}:\n\n`;
+    response += `Vehicle: ${vehicle} ${year}\n\n`
   }
 
-  response += "Available options:\n\n";
+  if (fitment) {
 
-  products.forEach((p, index) => {
-    response += `${index + 1}. ${p.title}\n`;
-  });
+    response += `Correct ${part} specification:\n`
 
-  return response;
+    for (const key in fitment) {
+      response += `${key}: ${fitment[key]}"\n`
+    }
+
+    response += "\n"
+  }
+
+  response += "Available products:\n\n"
+
+  products.forEach((p, i) => {
+    response += `${i + 1}. ${p.title}\n`
+  })
+
+  return response
 }
-
-module.exports = {
-  processMessage
-};
