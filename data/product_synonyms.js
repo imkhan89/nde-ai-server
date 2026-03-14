e1acd313f8fb9c2a0249698c5b3928d90e1ad9e9
imkhan89 <info@ndestore.com>
2026-03-15 03:52:31 +0500

    Create product_synonyms.js

---
+++ data/product_synonyms.js
@@ -0,0 +1,55 @@
+export const PRODUCT_SYNONYMS = {
+
+    wiper: [
+        "wiper",
+        "wipers",
+        "windshield wiper",
+        "windshield blade",
+        "vipar",
+        "vipr",
+        "viper",
+        "blade"
+    ],
+
+    brake: [
+        "brake",
+        "break",
+        "brakes",
+        "brake pad",
+        "brake pads",
+        "disc pad",
+        "pad"
+    ],
+
+    filter: [
+        "filter",
+        "filters",
+        "filtr",
+        "air filter",
+        "oil filter",
+        "ac filter",
+        "cabin filter"
+    ],
+
+    horn: [
+        "horn",
+        "car horn",
+        "denso horn",
+        "dual horn"
+    ],
+
+    coolant: [
+        "coolant",
+        "radiator coolant",
+        "engine coolant",
+        "antifreeze"
+    ],
+
+    sparkplug: [
+        "spark plug",
+        "spark plugs",
+        "plug",
+        "ignition plug"
+    ]
+
+};
