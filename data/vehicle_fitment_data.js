91fadbb5ea4768bd4e2494b175d356d8578e3540
imkhan89 <info@ndestore.com>
2026-03-15 04:04:36 +0500

    Update vehicle_fitment_data.js

--- data/vehicle_fitment_data.js
+++ data/vehicle_fitment_data.js
@@ -1,3 +1,5 @@
+// data/vehicle_fitment_data.js
+
 export const VEHICLE_FITMENT_DATA = {
 
     "toyota_corolla_2008_2026": {
@@ -27,6 +29,126 @@ export const VEHICLE_FITMENT_DATA = {
         cabin_filter: "87139-YZZ20",
 
         oil_filter: "90915-YZZF2"
+
+    },
+
+    "toyota_yaris_2014_2022": {
+
+        wiper: {
+            driver: "26\"",
+            passenger: "14\""
+        },
+
+        air_filter: "17801-21060",
+
+        cabin_filter: "87139-YZZ20",
+
+        oil_filter: "90915-YZZF2"
+
+    },
+
+    "toyota_hilux_2016_2026": {
+
+        wiper: {
+            driver: "22\"",
+            passenger: "22\""
+        },
+
+        air_filter: "17801-0L040",
+
+        cabin_filter: "87139-0K060",
+
+        oil_filter: "90915-YZZD2"
+
+    },
+
+    "honda_civic_2017_2021": {
+
+        wiper: {
+            driver: "26\"",
+            passenger: "18\""
+        },
+
+        air_filter: "17220-5AA-A00",
+
+        cabin_filter: "80292-TBA-A01",
+
+        oil_filter: "15400-RTA-003"
+
+    },
+
+    "honda_city_2015_2020": {
+
+        wiper: {
+            driver: "26\"",
+            passenger: "14\""
+        },
+
+        air_filter: "17220-5R0-008",
+
+        cabin_filter: "80292-TF0-G01",
+
+        oil_filter: "15400-RTA-003"
+
+    },
+
+    "suzuki_cultus_2017_2026": {
+
+        wiper: {
+            driver: "24\"",
+            passenger: "16\""
+        },
+
+        air_filter: "13780-79J00",
+
+        cabin_filter: "95860-74P00",
+
+        oil_filter: "16510-61A31"
+
+    },
+
+    "suzuki_swift_2018_2026": {
+
+        wiper: {
+            driver: "24\"",
+            passenger: "16\""
+        },
+
+        air_filter: "13780-79J00",
+
+        cabin_filter: "95860-74P00",
+
+        oil_filter: "16510-61A31"
+
+    },
+
+    "kia_sportage_2017_2022": {
+
+        wiper: {
+            driver: "26\"",
+            passenger: "16\""
+        },
+
+        air_filter: "28113-D3300",
+
+        cabin_filter: "97133-D3000",
+
+        oil_filter: "26300-35505"
+
+    },
+
+    "hyundai_santro_2000_2009": {
+
+        wiper: {
+            driver: "21\"",
+            passenger: "19\""
+        },
+
+        air_filter: "28113-02500",
+
+        cabin_filter: null,
+
+        oil_filter: "26300-35503"
 
     }
 
