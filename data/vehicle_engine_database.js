cff1522567db710f633dff105688d749a7e4014d
imkhan89 <info@ndestore.com>
2026-03-15 04:27:15 +0500

    Create vehicle_engine_database.js

---
+++ data/vehicle_engine_database.js
@@ -0,0 +1,23 @@
+export const VEHICLE_ENGINE_DATABASE = {
+
+    "toyota_corolla_2014_2019": [
+        "1.3L",
+        "1.6L",
+        "1.8L"
+    ],
+
+    "toyota_corolla_2008_2013": [
+        "1.3L",
+        "1.6L"
+    ],
+
+    "honda_civic_2016_2021": [
+        "1.5L Turbo",
+        "1.8L"
+    ],
+
+    "suzuki_cultus_2017_2024": [
+        "1.0L"
+    ]
+
+};
