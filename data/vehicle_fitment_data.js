5f83e060dc77e928bae531c9a74b5445a12a9f28
imkhan89 <info@ndestore.com>
2026-03-15 03:55:21 +0500

    Create vehicle_fitment_data.js

---
+++ data/vehicle_fitment_data.js
@@ -0,0 +1,33 @@
+export const VEHICLE_FITMENT_DATA = {
+
+    "toyota_corolla_2008_2026": {
+
+        wiper: {
+            driver: "26\"",
+            passenger: "14\""
+        },
+
+        air_filter: "17801-0T020",
+
+        cabin_filter: "87139-YZZ20",
+
+        oil_filter: "90915-YZZF2"
+
+    },
+
+    "toyota_vios_2013_2022": {
+
+        wiper: {
+            driver: "24\"",
+            passenger: "14\""
+        },
+
+        air_filter: "17801-21060",
+
+        cabin_filter: "87139-YZZ20",
+
+        oil_filter: "90915-YZZF2"
+
+    }
+
+};
