08190a08f52a90eb2c38970c761c7218fc48733e
imkhan89 <info@ndestore.com>
2026-03-15 03:42:01 +0500

    Create vehicle_generation_ranges.js

---
+++ data/vehicle_generation_ranges.js
@@ -0,0 +1,24 @@
+export const vehicleGenerationRanges = {
+
+  corolla: [
+    {
+      start: 2008,
+      end: 2026
+    }
+  ],
+
+  civic: [
+    {
+      start: 2016,
+      end: 2025
+    }
+  ],
+
+  hilux: [
+    {
+      start: 2015,
+      end: 2024
+    }
+  ]
+
+}
