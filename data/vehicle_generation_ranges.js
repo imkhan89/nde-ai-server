e5d66bde54eb7a3dffbae03fb54ea50a68e5b0be
imkhan89 <info@ndestore.com>
2026-03-15 04:03:00 +0500

    Update vehicle_generation_ranges.js

--- data/vehicle_generation_ranges.js
+++ data/vehicle_generation_ranges.js
@@ -1,11 +1,69 @@
+// data/vehicle_generation_ranges.js
+
 export const VEHICLE_GENERATION_RANGES = {
 
     "toyota_corolla": [
+        { start: 2002, end: 2007 },
         { start: 2008, end: 2026 }
     ],
 
     "toyota_vios": [
+        { start: 2008, end: 2012 },
         { start: 2013, end: 2022 }
+    ],
+
+    "toyota_yaris": [
+        { start: 2006, end: 2013 },
+        { start: 2014, end: 2022 }
+    ],
+
+    "toyota_hilux": [
+        { start: 2005, end: 2015 },
+        { start: 2016, end: 2026 }
+    ],
+
+    "toyota_fortuner": [
+        { start: 2005, end: 2015 },
+        { start: 2016, end: 2026 }
+    ],
+
+    "honda_civic": [
+        { start: 2006, end: 2012 },
+        { start: 2013, end: 2016 },
+        { start: 2017, end: 2021 },
+        { start: 2022, end: 2026 }
+    ],
+
+    "honda_city": [
+        { start: 2009, end: 2014 },
+        { start: 2015, end: 2020 },
+        { start: 2021, end: 2026 }
+    ],
+
+    "suzuki_cultus": [
+        { start: 2000, end: 2016 },
+        { start: 2017, end: 2026 }
+    ],
+
+    "suzuki_swift": [
+        { start: 2010, end: 2017 },
+        { start: 2018, end: 2026 }
+    ],
+
+    "suzuki_alto": [
+        { start: 2000, end: 2012 },
+        { start: 2013, end: 2018 },
+        { start: 2019, end: 2026 }
+    ],
+
+    "hyundai_santro": [
+        { start: 2000, end: 2009 }
+    ],
+
+    "kia_sportage": [
+        { start: 2010, end: 2016 },
+        { start: 2017, end: 2022 },
+        { start: 2023, end: 2026 }
     ]
 
 };
