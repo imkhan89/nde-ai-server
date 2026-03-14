295391108c80998b67e8fb2d13cfcc55aa18d1d1
imkhan89 <info@ndestore.com>
2026-03-15 03:26:48 +0500

    Update ai_engine.js

--- services/ai_engine.js
+++ services/ai_engine.js
@@ -1,82 +1,55 @@
 import { detectVehicle } from "./vehicle_parser.js"
 import { detectYear } from "./year_parser.js"
-import { detectPart } from "./part_parser.js"
+import { detectProductName } from "./product_name_parser.js"
+import { detectMake } from "./make_detector.js"
 import { getFitment } from "./fitment_engine.js"
 import { searchProducts } from "./product_search.js"
 
 export async function processMessage(message) {
 
-  const vehicleModel = detectVehicle(message)
+  const model = detectVehicle(message)
   const year = detectYear(message)
-  const part = detectPart(message)
+  const product = detectProductName(message)
 
-  // Detect make based on model
-  let make = null
+  const make = detectMake(model)
 
-  const toyotaModels = [
-    "corolla",
-    "yaris",
-    "hilux",
-    "vigo",
-    "revo",
-    "fortuner",
-    "prado"
-  ]
-
-  const hondaModels = [
-    "civic",
-    "city"
-  ]
-
-  if (toyotaModels.includes(vehicleModel)) {
-    make = "Toyota"
-  }
-
-  if (hondaModels.includes(vehicleModel)) {
-    make = "Honda"
-  }
-
-  if (!vehicleModel && !part) {
+  if (!model && !product) {
     return "Please tell us the vehicle and auto part you need."
   }
 
-  const fitment = getFitment(vehicleModel, year, part)
+  const fitment = getFitment(model, year, product)
 
-  // Intelligent search query
-  const searchQuery = vehicleModel
-    ? `${vehicleModel} ${part || ""}`
-    : part || message
+  const searchQuery = model
+    ? `${model} ${product || ""}`
+    : product || message
 
   const products = await searchProducts(searchQuery)
 
   let response = ""
 
-  if (make || vehicleModel || year) {
+  response += `Vehicle Identification:\n`
 
-    response += `Vehicle Details:\n`
-
-    if (make) {
-      response += `Make: ${make}\n`
-    }
-
-    if (vehicleModel) {
-      response += `Model: ${vehicleModel}\n`
-    }
-
-    if (year) {
-      response += `Year: ${year}\n`
-    }
-
-    response += "\n"
+  if (make) {
+    response += `Make: ${make}\n`
   }
 
-  if (part) {
-    response += `Product Requested: ${part}\n\n`
+  if (model) {
+    response += `Model: ${model}\n`
   }
 
-  if (fitment && part) {
+  if (year) {
+    response += `Year: ${year}\n`
+  }
 
-    response += `Correct ${part} specification:\n`
+  response += "\n"
+
+  if (product) {
+    response += `Product Requested: ${product}\n\n`
+  }
+
+  if (fitment && product) {
+
+    response += `Correct ${product} specification:\n`
 
     for (const key in fitment) {
       response += `${key}: ${fitment[key]}"\n`
@@ -93,4 +66,8 @@ export async function processMessage(message) {
   response += "Available products:\n\n"
 
   products.forEach((p, i) => {
-    response += `${i + 1}. ${p.tit
+    response += `${i + 1}. ${p.title}\n`
+  })
+
+  return response
+}
