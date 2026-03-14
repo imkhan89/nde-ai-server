8f13d89ede2660268ff2f1ded938dbdeeb89e015
imkhan89 <info@ndestore.com>
2026-03-15 04:27:36 +0500

    Create part_compatibility_rules.js

---
+++ data/part_compatibility_rules.js
@@ -0,0 +1,29 @@
+export const PART_COMPATIBILITY_RULES = {
+
+    wiper_blade: {
+        compatible_positions: [
+            "front_driver",
+            "front_passenger"
+        ]
+    },
+
+    brake_pad: {
+        compatible_positions: [
+            "front",
+            "rear"
+        ]
+    },
+
+    air_filter: {
+        location: "engine"
+    },
+
+    cabin_filter: {
+        location: "cabin"
+    },
+
+    oil_filter: {
+        location: "engine"
+    }
+
+};
