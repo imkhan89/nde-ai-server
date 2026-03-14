29c622f9a2120fb3acffaa51f2dcae19f5e69636
imkhan89 <info@ndestore.com>
2026-03-15 03:45:07 +0500

    Create spelling_dictionary.js

---
+++ data/spelling_dictionary.js
@@ -0,0 +1,16 @@
+export const spellingDictionary = {
+
+  corola: "corolla",
+  corolla: "corolla",
+
+  vipar: "wiper",
+  vipr: "wiper",
+  viper: "wiper",
+
+  break: "brake",
+  brek: "brake",
+
+  fillter: "filter",
+  filtr: "filter"
+
+}
