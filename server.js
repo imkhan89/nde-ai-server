31d7574de07286ed9a4921fe9930c2a8f5cc4699
imkhan89 <info@ndestore.com>
2026-03-16 01:44:39 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-import "dotenv/config"
-import app from "./server/http_server.js"
+import express from "express";
+import dotenv from "dotenv";
+import whatsappWebhook from "./routes/whatsapp_webhook.js";
 
-const PORT = process.env.PORT || 8080
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/webhook", whatsappWebhook);
+
+app.get("/", (req, res) => {
+    res.send("NDE Automotive AI Server Running");
+});
 
 app.listen(PORT, () => {
-    console.log(`ndestore.com Automotive AI running on port ${PORT}`)
-})
+    console.log(`ndestore.com Automotive AI running on port ${PORT}`);
+});
