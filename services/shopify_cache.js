1a3f05338008f5c98458498824204c0826dac893
imkhan89 <info@ndestore.com>
2026-03-15 20:15:19 +0500

    Update shopify_cache.js

--- services/shopify_cache.js
+++ services/shopify_cache.js
@@ -1,17 +1,26 @@
 let productsCache = [];
 
+/*
+Store products after Shopify sync
+*/
 export function setCachedProducts(products) {
 
 productsCache = products || [];
 
 }
 
+/*
+Get all cached products
+*/
 export function getCachedProducts() {
 
 return productsCache;
 
 }
 
+/*
+Optional helper
+*/
 export function getProductCount(){
 
 return productsCache.length;
