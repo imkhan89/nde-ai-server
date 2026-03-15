ccc3d8243325333fbc72d7d733d3bef2c850dfd5
imkhan89 <info@ndestore.com>
2026-03-15 18:01:04 +0500

    Update whatsapp_webhook.js

--- routes/whatsapp_webhook.js
+++ routes/whatsapp_webhook.js
@@ -1,23 +1,42 @@
 import express from "express"
 import { processUserMessage } from "../services/ai_engine.js"
+import twilio from "twilio"
 
 const router = express.Router()
 
-router.post("/whatsapp", async (req, res) => {
+async function handleWhatsapp(req, res) {
 
-  const message = req.body.Body || ""
+  try {
 
-  const reply = await processUserMessage(message)
+    const message = req.body.Body || ""
 
-  const twiml = `
-<Response>
-<Message>${reply}</Message>
-</Response>
-`
+    const reply = await processUserMessage(message)
 
-  res.type("text/xml")
-  res.send(twiml)
+    const twiml = new twilio.twiml.MessagingResponse()
 
-})
+    twiml.message(reply)
+
+    res.type("text/xml")
+    res.send(twiml.toString())
+
+  } catch (err) {
+
+    console.error(err)
+
+    const twiml = new twilio.twiml.MessagingResponse()
+
+    twiml.message("Server error. Please try again.")
+
+    res.type("text/xml")
+    res.send(twiml.toString())
+
+  }
+
+}
+
+router.post("/whatsapp", handleWhatsapp)
+
+/* support second endpoint Twilio might call */
+router.post("/", handleWhatsapp)
 
 export default router
