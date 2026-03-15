602397c8ee2dc698f60d14cff2b3ef0808175977
imkhan89 <info@ndestore.com>
2026-03-15 17:43:23 +0500

    Create brand.js

---
+++ config/brand.js
@@ -0,0 +1,20 @@
+const BRAND = {
+  name: "ndestore.com",
+  website: "https://www.ndestore.com",
+  domain: "www.ndestore.com",
+
+  description: "Global automotive parts, accessories and decals store",
+
+  service_area: "Worldwide",
+
+  international_support: true,
+
+  international_message:
+    "ndestore.com serves customers worldwide. Please share your country so we can assist with availability and shipping.",
+
+  support_email: "support@ndestore.com",
+
+  whatsapp: "+923087643288"
+}
+
+export default BRAND
