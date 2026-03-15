960e3fbc19030cbe6629859c70e41e5669a61458
imkhan89 <info@ndestore.com>
2026-03-15 17:40:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
-import dotenv from "dotenv"
-
-dotenv.config()
-
-export default {
-TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
-TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
-TWILIO_NUMBER: process.env.TWILIO_NUMBER,
-
-SHOPIFY_STORE: process.env.SHOPIFY_STORE,
-SHOPIFY_ACCESS_TOKEN: process.env.SHOPIFY_ACCESS_TOKEN,
-
-ADMIN_PORT: process.env.ADMIN_PORT || 4000,
-SERVER_PORT: process.env.SERVER_PORT || 3000
+const BRAND = {
+  name: "ndestore.com",
+  website: "https://www.ndestore.com",
+  domain: "www.ndestore.com",
+  support_email: "support@ndestore.com",
+  whatsapp: "+923087643288",
+  description: "Auto Parts, Accessories and Decals store"
 }
+
+export default BRAND
