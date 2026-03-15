35803dd8530207e4777078ed14a017b8607a43ba
imkhan89 <info@ndestore.com>
2026-03-15 09:24:45 +0500

    Create error_handler.js

---
+++ middleware/error_handler.js
@@ -0,0 +1,10 @@
+export default function errorHandler(err, req, res, next) {
+
+  console.error("Server Error:", err)
+
+  res.status(err.status || 500).json({
+    success: false,
+    error: err.message || "Internal Server Error"
+  })
+
+}
