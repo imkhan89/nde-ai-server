2970e4bcdc241aad9b48d7e951855d519e13d1f9
imkhan89 <info@ndestore.com>
2026-03-15 18:50:42 +0500

    Update brand.js

--- config/brand.js
+++ config/brand.js
@@ -1,20 +1,6 @@
-const BRAND = {
-  name: "ndestore.com",
-  website: "https://www.ndestore.com",
-  domain: "www.ndestore.com",
+export const BRAND_NAME = "ndestore.com";
+export const WEBSITE_URL = "https://www.ndestore.com";
 
-  description: "Global automotive parts, accessories and decals store",
-
-  service_area: "Worldwide",
-
-  international_support: true,
-
-  international_message:
-    "ndestore.com serves customers worldwide. Please share your country so we can assist with availability and shipping.",
-
-  support_email: "support@ndestore.com",
-
-  whatsapp: "+923087643288"
+export function brandFooter() {
+  return `\n\nPowered by ndestore.com\n${WEBSITE_URL}`;
 }
-
-export default BRAND
