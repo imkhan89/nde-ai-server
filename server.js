128bf6cf590df45efb85f729afef8c291972329b
imkhan89 <info@ndestore.com>
2026-03-16 01:55:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-import whatsappRouter from "./routes/whatsapp_webhook.js";
+
+import whatsappWebhook from "./routes/whatsapp_webhook.js";
+import startShopifySync from "./services/shopify_sync.js";
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
-/* Twilio sends form encoded data */
+/* Twilio body format */
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-/* Register webhook route */
-app.use("/webhook", whatsappRouter);
+/* Start Shopify Sync */
+startShopifySync();
 
-/* Root test route */
+/* WhatsApp webhook */
+app.use("/webhook", whatsappWebhook);
+
 app.get("/", (req, res) => {
   res.send("NDE Automotive AI Server Running");
 });
