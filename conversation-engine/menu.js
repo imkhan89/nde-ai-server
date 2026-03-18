9d0123ec1a4f3e1fa8f612a5a5148d827bb6368b
imkhan89 <info@ndestore.com>
2026-03-19 01:53:53 +0500

    Create menu.js

---
+++ conversation-engine/menu.js
@@ -0,0 +1,17 @@
+// conversation-engine/menu.js
+
+function getMainMenu() {
+  return `Welcome to NDESTORE 🚗
+
+Please choose an option:
+
+1️⃣ Auto Parts Search
+2️⃣ Car Accessories
+3️⃣ Order Support
+
+Reply with a number to continue.`;
+}
+
+module.exports = {
+  getMainMenu
+};
