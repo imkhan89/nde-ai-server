37d99c957115a9630c15b4bc3a8502454c1e8401
imkhan89 <info@ndestore.com>
2026-03-18 23:02:22 +0500

    Create mapper.js

---
+++ shopify-engine/mapper.js
@@ -0,0 +1,28 @@
+// shopify-engine/mapper.js
+
+function buildSearchQuery(vehicle) {
+  const { make, model, year, part } = vehicle;
+
+  let query = '';
+
+  if (part) query += part + ' ';
+  if (make) query += make + ' ';
+  if (model) query += model + ' ';
+  if (year) query += year;
+
+  return query.trim();
+}
+
+function generateProductLink(vehicle) {
+  const baseUrl = 'https://ndestore.com/search?q=';
+
+  const query = buildSearchQuery(vehicle);
+
+  const encoded = encodeURIComponent(query);
+
+  return `${baseUrl}${encoded}`;
+}
+
+module.exports = {
+  generateProductLink
+};
